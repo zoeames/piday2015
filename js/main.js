ccd46a7c0b89b79e0a2477fcbbfcd02ea9b80970
Zoe Ames <zoeames@gmail.com>
2015-03-14 10:04:38 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//What is the phase of the moon tonight?
+
+function moonPhase() {
+
+}
+moonPhase();
+
+
+//In what year was the record low temperature for today's date?  use almanac
+function recordLow() {
+
+}
+recordLow();
+
+
+//ASSIGNMENT:  write a function that pulls out the current temp after you add a zipcode to a form and hit submit.  It should then display the results in the html body. Apply some nice CSS.
