faf53735ec8508e34f09ab1633db48ee56bdc694
Zoe Ames <zoeames@gmail.com>
2015-03-22 23:49:30 -0500

    server changed

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function(req, res) {
+  res.render('index');
 });
 
 app.listen(app.get('port'), function() {
